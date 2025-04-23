3ea74057f973e6f6c24a24f3980a25a7c0f0709e
adarsh <adarshn54688@gmail.com>
2025-04-23 08:43:01 +0530

    removed a file

--- circulargen/generator/urls.py
+++ circulargen/generator/urls.py
@@ -5,7 +5,7 @@ from generator import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('generate_circular/', views.generate_circular, name='generate_circular'),
-    # path('send_whatsapp/', views.send_whatsapp, name='send_whatsapp'),
     path('send-email/', views.send_email, name='send_email'),
+    # path('send-email/', views.send_email, name='send_email'),
 
 ]
