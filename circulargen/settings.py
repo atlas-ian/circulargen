a0fc3e6c312dde7508f343fc6da3dbe5d8bf6294
adarshaadi06 <adarshn54688@gmail.com>
2025-07-28 16:26:44 +0530

    edited some files

--- circulargen/settings.py
+++ circulargen/settings.py
@@ -102,9 +102,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles_root'
 EMAIL_BACKEND      = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST         = 'smtp.gmail.com'
 EMAIL_PORT         = 587
+EMAIL_USE_TLS      = True
 EMAIL_HOST_USER    = 'circular@bietdvg.edu'
 EMAIL_HOST_PASSWORD= 'nzdj iedd gjry ovet'
-EMAIL_USE_TLS      = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
